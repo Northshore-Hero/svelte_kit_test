91e50c55641bee0496c2dacc1736c06e1541d9a6
Daniel Mattila <danmattila99@gmail.com>
2022-12-29 16:05:02 -0600

    Initial commit

---
+++ src/lib/Download.svelte
@@ -0,0 +1,26 @@
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<script lang="ts">
+    let downloadMsg = "";
+    let url = "README.md";
+    let fileName = "README.md";
+
+    async function download(){
+        const anchorElement = document.createElement('a');
+        anchorElement.href = url;
+        anchorElement.download = fileName ?? '';
+        anchorElement.click();
+        anchorElement.remove();
+        downloadMsg = "Downloaded the file!"
+    }
+</script>
+
+<div>
+    <div class="row">
+        <button on:click={download} class="btn"><i class="fa fa-download"></i>
+            Download
+        </button>
+    </div>
+    <p>{downloadMsg}</p>
+</div>

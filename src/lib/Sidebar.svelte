8d29e49ffed2f43def84541f014de797bb1a4038
Daniel Mattila <danmattila99@gmail.com>
2022-12-29 22:26:58 -0600

    Fixed unnecesary scroll area at bottom of page

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <Sidebar class="w-full ">
-    <SidebarWrapper>
-        <SidebarGroup class="lg:h-screen">
+    <SidebarWrapper class="lg:h-screen">
+        <SidebarGroup>
             <SidebarItem label="Dashboard">
                 <svelte:fragment slot="icon">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" /><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" /></svg>
